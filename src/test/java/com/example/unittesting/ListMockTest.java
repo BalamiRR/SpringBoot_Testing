1532baf95afbe195c9e76149b070b24db3e71160
fuat <fuatkara.95@gmail.com>
2020-06-17 13:40:39 +0200

    Spring Boot Testing

--- src/test/java/com/example/unittesting/ListMockTest.java
+++ src/test/java/com/example/unittesting/ListMockTest.java
@@ -1,19 +1,18 @@
 package com.example.unittesting;
 
-import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.anything;
-import static org.hamcrest.CoreMatchers.any;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 class ListMockTest {
@@ -65,9 +64,52 @@ class ListMockTest {
 		verify(mock, atMost(2)).get(5);
 		verify(mock, never()).get(2);
 		
-		
-		
 	}
 
+	
+	@Test
+	public  void argumentCapturing() {
+		
+		List mock = Mockito.mock(List.class);
+		
+		//SUT
+		mock.add("SomeString1");
+		mock.add("SomeString2");
+		
+		//Verification
+		ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+		verify(mock, times(2)).add(captor.capture());
+		
+		List<String> allValues = captor.getAllValues();
+		
+		assertEquals("SomeString", allValues.get(0));
+		assertEquals("SomeString", allValues.get(1));
+	}
+	
 
+	@Test
+	public void spying() {
+		ArrayList arrayListSpy = spy(ArrayList.class);
+		arrayListSpy.add("Test0");
+		
+		System.out.println(arrayListSpy.get(0));  // Test0
+		System.out.println(arrayListSpy.size()); // 1 olucak
+		arrayListSpy.add("Test");
+		arrayListSpy.add("Test2");
+		System.out.println(arrayListSpy.get(1));  // Test yazisini alicak
+		System.out.println(arrayListSpy.size());  // 3 olucak
+		
+		when(arrayListSpy.size()).thenReturn(5);
+		System.out.println(arrayListSpy.size()); // 5 olucak
+		
+		arrayListSpy.add("Test4");
+		System.out.println(arrayListSpy.size()); // 5 olucak
+		
+	}
+	
+	
+	
+	
+	
 }
+
